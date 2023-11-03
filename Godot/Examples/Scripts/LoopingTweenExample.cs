44809f1955f3eee37451eaf2488d4461e220e7cd
Guillemsc <gsunyercaldu@gmail.com>
2023-11-03 16:48:17 +0100

    Improved example

--- Godot/Examples/Scripts/LoopingTweenExample.cs
+++ Godot/Examples/Scripts/LoopingTweenExample.cs
@@ -1,4 +1,5 @@
 ﻿using Godot;
+using GTweens.Enums;
 using GTweens.Tweens;
 using GTweensGodot.Extensions;
 
@@ -8,11 +9,12 @@ public partial class LoopingTweenExample : Node
 {
     [Export] public Node2D Target;
     [Export] public int Loops;
+    [Export] public ResetMode ResetMode = ResetMode.InitialValues;
 	
     public override void _Ready()
     {
         GTween tween = Target.TweenPositionX(150, 1);
-        tween.SetLoops(Loops);
+        tween.SetLoops(Loops, ResetMode);
         tween.Play();
     }
 }
